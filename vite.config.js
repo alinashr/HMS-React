ad46f4dcc7e4ce450948aa00b3f0036cef0b2827
alinashr <108988055+alinashr@users.noreply.github.com>
2025-03-10 22:22:39 +0545

    Third

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,8 @@ import react from '@vitejs/plugin-react'
 
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+  // plugins: [react()],
   plugins: [react(), tailwindcss()],
+  base: '/HMS-React/' 
+  
 });
